ab331668285413154c78811310135fdf659d11da
Ramil0001 <136189542+Ramil0001@users.noreply.github.com>
2023-06-10 21:43:19 +0400

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,13 +23,13 @@ export default function Index() {
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
-          RamuS Davet Et
+          Davet Et
               </a>
             </Link>
   <div className="py-10"></div>
   <Link href={"https://bit.ly/3LYzaDe"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote RamuS</a>
+Oy Ver </a>
   </Link>
         </div>
 
